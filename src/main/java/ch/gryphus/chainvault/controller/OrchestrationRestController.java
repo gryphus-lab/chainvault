bb90edeb23cfe2f4e5743a06eeecb7e41e198b21
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2026-02-23 18:58:05 +0100

    Fix tests

--- src/main/java/ch/gryphus/chainvault/controller/OrchestrationRestController.java
+++ src/main/java/ch/gryphus/chainvault/controller/OrchestrationRestController.java
@@ -4,7 +4,10 @@ import ch.gryphus.chainvault.service.OrchestrationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
@@ -21,6 +24,6 @@ public class OrchestrationRestController {
     @PostMapping(value="/process")
     public ResponseEntity<Object> startProcessInstance(@RequestBody Map<String, Object> payload) {
         String retVal = orchestrationService.startProcess(payload);
-        return ResponseEntity.status(HttpStatus.CREATED).body("Process instance started for payload:" + payload + " with id:" + retVal);
+        return ResponseEntity.status(HttpStatus.CREATED).body("Process instance started for payload:%s with id:%s".formatted(payload, retVal));
     }
 }
--- src/test/java/ch/gryphus/chainvault/controller/OrchestrationRestControllerTest.java
+++ src/test/java/ch/gryphus/chainvault/controller/OrchestrationRestControllerTest.java
@@ -1,7 +1,6 @@
 package ch.gryphus.chainvault.controller;
 
 import ch.gryphus.chainvault.service.OrchestrationService;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +10,6 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.assertj.MockMvcTester;
 
-import java.util.HashMap;
 import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;

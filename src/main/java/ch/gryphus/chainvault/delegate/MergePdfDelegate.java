f0525cfeca14c6fc78b4fd5b95333f7b4d6fa6ce
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2026-02-23 13:05:32 +0100

    Add delegates

--- src/main/java/ch/gryphus/chainvault/delegate/MergePdfDelegate.java
+++ src/main/java/ch/gryphus/chainvault/delegate/MergePdfDelegate.java
@@ -3,7 +3,7 @@ package ch.gryphus.chainvault.delegate;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.delegate.JavaDelegate;
 
-public class ExtractAndHashDelegate implements JavaDelegate {
+public class MergePdfDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) {
         //
--- src/main/java/ch/gryphus/chainvault/delegate/TransformMetadataDelegate.java
+++ src/main/java/ch/gryphus/chainvault/delegate/TransformMetadataDelegate.java
@@ -3,7 +3,7 @@ package ch.gryphus.chainvault.delegate;
 import org.flowable.engine.delegate.DelegateExecution;
 import org.flowable.engine.delegate.JavaDelegate;
 
-public class ExtractAndHashDelegate implements JavaDelegate {
+public class TransformMetadataDelegate implements JavaDelegate {
     @Override
     public void execute(DelegateExecution execution) {
         //

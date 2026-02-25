d4235f9cc06f4be3f283bdda820b1910879e3382
gryphus-lab <80040733+gryphus-lab@users.noreply.github.com>
2026-02-25 21:46:42 +0000

    feat: add springdoc OpenAPI dependency and OpenApiConfig

---
+++ src/main/java/ch/gryphus/chainvault/config/OpenApiConfig.java
@@ -0,0 +1,21 @@
+package ch.gryphus.chainvault.config;
+
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI chainVaultOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ChainVault API")
+                        .version("0.0.1")
+                        .description("APIs for ChainVault orchestration and migration service")
+                        .contact(new Contact().name("gryphus-lab").email("noreply@gryphus-lab")));
+    }
+}
